7e6d994d8cc71f21c56cb909744e658dba2b56ec
Marcin Jastrzębski <marcin.jastrzebski@outlook.com>
2020-08-21 17:40:20 +0200

    final pages v4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://stormy-tor-05209.herokuapp.com/v1/task/';
+  const apiRoot = 'https://stormy-tor-05209.herokuapp.com/v1/';
   const trelloApiRoot = 'https://stormy-tor-05209.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
